25cac5fb2b01e9d15e6c7032e4391dee551aa3ae
pinethx <pinethx0303@gmail.com>
2021-04-20 12:27:45 +0900

    service

--- src/main/java/grade_management_program/service/ScoreService.java
+++ src/main/java/grade_management_program/service/ScoreService.java
@@ -1,6 +1,9 @@
 package grade_management_program.service;
 
+import java.util.List;
+
 import grade_management_program.dao.impl.ScoreDaoImpl;
+import grade_management_program.dto.Score;
 
 public class ScoreService {
 	private ScoreDaoImpl dao;
@@ -9,4 +12,23 @@ public class ScoreService {
 		dao = ScoreDaoImpl.getInstance();
 	}
 
+	public List<Score> showScoreAll() {
+		return dao.selectScoreByAll();
+	}
+	
+	public void showScoreSubcode(Score score) {
+		dao.selectScoreByNo(score);
+	}
+	
+	public void createScore(Score score) {
+		dao.insertScore(score);
+	}
+	
+	public void delScore(int stdNo, int subNo) {
+		dao.deleteScore(stdNo, subNo);
+	}
+	
+	public void upScore(Score score) {
+		dao.updateScore(score);
+	}
 }
